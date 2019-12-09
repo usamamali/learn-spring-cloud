f6678010617fe8f214ef7347294fe8d93c1eb26a
Usama M. Ali <eng.usama.muhammad@gmail.com>
2019-12-09 14:31:50 +0100

    add config files + add client

---
+++ spring-cloud-config-client/src/test/java/com/spring/cloud/basics/client/SpringCloudConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.basics.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- spring-cloud-config-server/src/main/java/com/spring/cloud/basics/config/server/SpringCloudConfigApplication.java
+++ spring-cloud-config-server/src/main/java/com/spring/cloud/basics/config/server/SpringCloudConfigApplication.java
@@ -1,4 +1,4 @@
-package com.spring.cloud.basics.config;
+package com.spring.cloud.basics.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
