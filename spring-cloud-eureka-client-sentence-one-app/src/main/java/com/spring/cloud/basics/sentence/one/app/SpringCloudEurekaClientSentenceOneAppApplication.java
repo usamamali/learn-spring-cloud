2dc822c9b2c78d09cefbea5f8155d73c179af2bf
Usama M. Ali <eng.usama.muhammad@gmail.com>
2020-01-04 05:40:16 +0100

    add new app to service the sentence with different profiles

---
+++ spring-cloud-eureka-client-sentence-one-app/src/main/java/com/spring/cloud/basics/sentence/one/app/SpringCloudEurekaClientSentenceOneAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.basics.sentence.one.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaClientSentenceOneAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaClientSentenceOneAppApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-eureka-client-sentence-one-app/src/test/java/com/spring/cloud/basics/sentence/one/app/SpringCloudEurekaClientSentenceOneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.basics.sentence.one.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaClientSentenceOneAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
