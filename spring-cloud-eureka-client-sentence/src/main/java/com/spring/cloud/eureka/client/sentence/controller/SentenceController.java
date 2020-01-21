89ed52c2934dec8223500c956d69cab43deb106d
Usama M. Ali <eng.usama.muhammad@gmail.com>
2020-01-21 09:44:52 +0100

    add hystrix for circuit breaker

--- spring-cloud-eureka-client-sentence/src/main/java/com/spring/cloud/eureka/client/sentence/controller/SentenceController.java
+++ spring-cloud-eureka-client-sentence/src/main/java/com/spring/cloud/eureka/client/sentence/controller/SentenceController.java
@@ -1,29 +1,21 @@
 package com.spring.cloud.eureka.client.sentence.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.spring.cloud.eureka.client.sentence.service.SentenceService;
-
-import java.net.URI;
-import java.util.List;
 
 @RestController
 public class SentenceController {
 
-@Autowired
-SentenceService sentenceService;
+	@Autowired
+	SentenceService sentenceService;
 
 	@GetMapping("/sentence")
 	public @ResponseBody String getSentence() {
 		return sentenceService.buildSentence();
 	}
-
 
 }
--- spring-cloud-eureka-client-sentence/src/main/java/com/spring/cloud/eureka/client/sentence/service/WordServiceImpl.java
+++ spring-cloud-eureka-client-sentence/src/main/java/com/spring/cloud/eureka/client/sentence/service/WordServiceImpl.java
@@ -6,6 +6,7 @@ package com.spring.cloud.eureka.client.sentence.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.spring.cloud.eureka.client.sentence.dao.AdjectiveClient;
 import com.spring.cloud.eureka.client.sentence.dao.ArticleClient;
 import com.spring.cloud.eureka.client.sentence.dao.NounClient;
@@ -19,23 +20,24 @@ import com.spring.cloud.eureka.client.sentence.domain.Word;
  */
 @Service
 public class WordServiceImpl implements WordService {
-	
+
 	@Autowired
 	AdjectiveClient adjectiveClient;
-	
+
 	@Autowired
 	ArticleClient articleClient;
-	
+
 	@Autowired
 	NounClient nounClient;
-	
+
 	@Autowired
 	SubjectClient subjectClient;
-	
+
 	@Autowired
 	VerbClient verbClient;
 
 	@Override
+	@HystrixCommand(fallbackMethod = "fallbackSubject")
 	public Word getSubject() {
 		return new Word(subjectClient.getWord());
 	}
@@ -51,13 +53,27 @@ public class WordServiceImpl implements WordService {
 	}
 
 	@Override
+	@HystrixCommand(fallbackMethod = "fallbackAdjective")
 	public Word getAdjective() {
 		return new Word(adjectiveClient.getWord());
 	}
 
 	@Override
+	@HystrixCommand(fallbackMethod = "fallbackNoun")
 	public Word getNoun() {
 		return new Word(nounClient.getWord());
+	}
+
+	public Word fallbackAdjective() {
+		return new Word("");
+	}
+
+	public Word fallbackSubject() {
+		return new Word("Someone");
+	}
+
+	public Word fallbackNoun() {
+		return new Word("something");
 	}
 
 }
